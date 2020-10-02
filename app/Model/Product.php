f35374fbaa475ec8c0656be87391fd4cb38df739
1519176958@qq.com <1519176958@qq.com>
2020-10-02 13:16:49 +0800

    产品图片修改器

--- app/Model/Product.php
+++ app/Model/Product.php
@@ -7,4 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class Product extends Model {
 
     public $timestamps = false;
+
+    public function getCoverImageAttribute($value)
+    {
+        return env('APP_URL', 'http://api.suibian.ink') . $value;
+    }
 }
