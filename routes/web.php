8717057472a1e419c43dfe31a75f2acae01ebaa7
1519176958@qq.com <1519176958@qq.com>
2020-10-10 17:20:54 +0800

    修改路由

--- routes/web.php
+++ routes/web.php
@@ -62,49 +62,46 @@ $router->group(['middleware' => 'auth'], function () use ($router) {
         $router->post('delete', 'AddressController@delete');
     });
 
-    $router->group(['prefix' => 'admin', 'middleware' => 'admin_auth'], function () use ($router) {
-        $router->group(['prefix' => 'order'], function () use ($router) {
-            $router->post('show', 'OrderController@show');
-        });
-        $router->group(['prefix' => 'user'], function () use ($router) {
-            $router->post('show', 'UserController@show');
-        });
-        $router->group(['prefix' => 'category'], function () use ($router) {
-            $router->post('list', 'CategoryController@list');
-            $router->post('add', 'CategoryController@add');
-            $router->post('update', 'CategoryController@update');
 
-        });
-        $router->group(['prefix' => 'banner'], function () use ($router) {
-            $router->post('list', 'BannerController@list');
-            $router->post('add', 'BannerController@add');
-            $router->post('update', 'BannerController@update');
+});
 
-        });
+$router->group(['prefix' => 'admin', 'middleware' => 'admin_auth'], function () use ($router) {
+    $router->group(['prefix' => 'order'], function () use ($router) {
+        $router->post('show', 'OrderController@show');
+    });
+    $router->group(['prefix' => 'user'], function () use ($router) {
+        $router->post('show', 'UserController@show');
+    });
+    $router->group(['prefix' => 'category'], function () use ($router) {
+        $router->post('list', 'CategoryController@list');
+        $router->post('add', 'CategoryController@add');
+        $router->post('update', 'CategoryController@update');
 
-        $router->group(['prefix' => 'product'], function () use ($router) {
-            $router->post('list', 'ProductController@list');
-            $router->post('add', 'ProductController@add');
-            $router->post('update_stock', 'ProductController@update_stock');
-            $router->post('update', 'ProductController@update');
-            $router->post('modify_price', 'ProductController@modifyPrice');
-            $router->post('active', 'ProductController@active');
-
-
-
-
-        });
-
-        $router->group(['prefix' => 'image'], function () use ($router) {
-            $router->post('upload', 'ImageController@upload');
-
-        });
-
-        $router->group(['prefix' => 'admin'], function () use ($router) {
-            $router->post('login', 'AdminController@login');
-        });
+    });
+    $router->group(['prefix' => 'banner'], function () use ($router) {
+        $router->post('list', 'BannerController@list');
+        $router->post('add', 'BannerController@add');
+        $router->post('update', 'BannerController@update');
 
     });
 
+    $router->group(['prefix' => 'product'], function () use ($router) {
+        $router->post('list', 'ProductController@list');
+        $router->post('add', 'ProductController@add');
+        $router->post('update_stock', 'ProductController@update_stock');
+        $router->post('update', 'ProductController@update');
+        $router->post('modify_price', 'ProductController@modifyPrice');
+        $router->post('active', 'ProductController@active');
+    });
+
+    $router->group(['prefix' => 'image'], function () use ($router) {
+        $router->post('upload', 'ImageController@upload');
+
+    });
 });
+
+$router->group(['prefix' => 'admin'], function () use ($router) {
+    $router->post('login', 'AdminController@login');
+});
+
 
